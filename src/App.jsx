3cabc9e05831aedfa15fdad1b68163457d7d0246
allsun <869472104@qq.com>
2023-07-17 23:48:36 +0800

    底部导航添加部分代码

--- src/App.jsx
+++ src/App.jsx
@@ -9,18 +9,20 @@ import routes from '@/router'
 import { ConfigProvider } from 'zarm'
 import zhCN from 'zarm/lib/config-provider/locale/zh_CN'
 import 'zarm/dist/zarm.css'
-
+import NavBar from '@/components/NavBar';
 
 function App() {
   return <Router>
-    <ConfigProvider primaryColor={'#007fff'} locale={zhCN}>
+    <ConfigProvider primaryColor={'#007fff'}>
       <>
        <Routes>
         {routes.map(route => <Route exact key={route.path} path={route.path} element={<route.component />} />)}
        </Routes>
-      </>
+      <NavBar showNav={showNav} />
+     </>
     </ConfigProvider>
-  </Router> 
+    <NavBar showNav={true} />
+  </Router>
 
 }
 
